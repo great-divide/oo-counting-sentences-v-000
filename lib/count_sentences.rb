9bd5f8c29c8103779405bcb5b87cab320a93a3e4
Brian Birdwell <bbirdwell@gmail.com>
2018-10-03 06:39:18 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -24,8 +24,8 @@ class String
   end
 
   def count_sentences
-    array = self.split!(/.?!/)
-    array.delete("")
+    array = self.split(/.?!/)
+    # array.delete("")
     array.length
   end
 end
